9b2bccde41f0eb0a9658e2897d0e3f6ed0565f15
jonyhy96 <hy352144278@gmail.com>
2022-06-14 11:21:43 +0800

    stargz: ignore NumLink for json

--- stargz/stargz.go
+++ stargz/stargz.go
@@ -166,7 +166,7 @@ type TOCEntry struct {
 
 	// NumLink is the number of entry names pointing to this entry.
 	// Zero means one name references this entry.
-	NumLink int
+	NumLink int `json:"-"`
 
 	// Xattrs are the extended attribute for the entry.
 	Xattrs map[string][]byte `json:"xattrs,omitempty"`
